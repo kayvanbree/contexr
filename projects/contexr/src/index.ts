1ecd96e497dd83c50e54d8391dd6f842e15b3436
Unknown <kayvanbree@gmail.com>
2018-10-07 13:55:43 +0200

    Export context menu item

--- projects/contexr/src/index.ts
+++ projects/contexr/src/index.ts
@@ -2,5 +2,6 @@
  * Public API Surface of contexr
  */
 
+export * from './lib/types/context-menu-item';
 export * from './lib/components/context-menu/context-menu.component';
 export * from './lib/contexr.module';
