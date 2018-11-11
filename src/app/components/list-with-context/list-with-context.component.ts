8237e171c7d4f83822ea02487022d7a3955cb62d
Unknown <kayvanbree@gmail.com>
2018-11-11 21:56:10 +0100

    #24: Improve update site

--- src/app/components/list-with-context/list-with-context.component.ts
+++ src/app/components/list-with-context/list-with-context.component.ts
@@ -9,9 +9,9 @@ import {MatTableDataSource} from '@angular/material';
 })
 export class ListWithContextComponent implements OnInit {
 
-  @Input() elements: any;
+  @Input() elements: any[];
 
-  dataSource: any;
+  dataSource: MatTableDataSource<any>;
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
@@ -30,8 +30,16 @@ export class ListWithContextComponent implements OnInit {
       text: 'Select element',
       context: ['element'],
       action: (args: any) => {
-        args.list.selected = args.element;
+        args.list.onSelectRow(null, args.element);
       }
+    },
+    {
+      text: 'Delete element',
+      context: ['element'],
+      action: (args: any) => {
+        args.list.removeElement(args.element);
+      },
+      hotkey: 'del'
     }
   ];
 
@@ -41,6 +49,23 @@ export class ListWithContextComponent implements OnInit {
 
   public ngOnInit(): void {
     this.dataSource = new MatTableDataSource(this.elements);
+  }
+
+  public onSelectRow(event: MouseEvent, row: any): void {
+    if (event && event.ctrlKey) {
+      this.selected.push(row);
+    } else {
+      this.selected = [row];
+    }
+  }
+
+  private removeElement(element) {
+    console.log('Removing element');
+    console.log(element);
+    const index = this.elements.indexOf(element);
+    if (index > -1) {
+      this.elements.splice(index, 1);
+    }
   }
 
   private addElement() {
