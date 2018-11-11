5fc5ff822bf7968ab097a98ebdf90751ae18eaf8
Unknown <kayvanbree@gmail.com>
2018-11-11 20:03:14 +0100

    #24: Little refactor

--- projects/contexr/src/lib/components/context-menu/context-menu.component.ts
+++ projects/contexr/src/lib/components/context-menu/context-menu.component.ts
@@ -17,15 +17,14 @@ export class ContextMenuComponent implements OnInit, OnDestroy {
   private contextStateSub: Subscription;
 
   constructor(public contexr: ContexrService) {
-    // Event capturing (not possible in real Angular yet)
-    document.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.contexr.reset();
-    }, true);
-    document.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      this.contexr.reset();
-    }, true);
+    // Event capturing by setting options to true (not possible in real Angular yet)
+    document.addEventListener('click', this.resetContext, true);
+    document.addEventListener('contextmenu', this.resetContext, true);
+  }
+
+  resetContext(event): void {
+    event.preventDefault();
+    this.contexr.reset();
   }
 
   /**
