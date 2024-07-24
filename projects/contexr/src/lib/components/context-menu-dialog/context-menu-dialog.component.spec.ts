02eabeab1f322e08adc8c921a348449a4faf7e63
Scubakay <=>
2024-07-24 21:24:09 +0200

    #75 Add tests for ContextMenuDialogComponent

--- projects/contexr/src/lib/components/context-menu-dialog/context-menu-dialog.component.spec.ts
+++ projects/contexr/src/lib/components/context-menu-dialog/context-menu-dialog.component.spec.ts
@@ -5,10 +5,13 @@ import { CONTEXT_STATE } from '../../types/context-state';
 import { MenuItem } from '../../types/menu-item';
 import { HotkeysServiceMock } from '../../mocks/hotkeys-service.mock';
 import { HotkeysService } from 'angular2-hotkeys';
+import { ContexrService } from '../../providers/contexr.service';
+import { ContexrServiceMock } from '../../mocks/contexr-service.mock';
 
 describe('ContextMenuDialogComponent', () => {
   let component: ContextMenuDialogComponent;
   let fixture: ComponentFixture<ContextMenuDialogComponent>;
+  let contexrMock = new ContexrServiceMock();
 
   beforeEach(async () => {
     let items: MenuItem[] = [];
@@ -17,7 +20,8 @@ describe('ContextMenuDialogComponent', () => {
       imports: [ContextMenuDialogComponent],
       providers: [
         { provide: HotkeysService, useClass: HotkeysServiceMock },
-        { provide: CONTEXT_STATE, useValue: items }
+        { provide: CONTEXT_STATE, useValue: items },
+        { provide: ContexrService, useValue: contexrMock }
       ]
     })
     .compileComponents();
@@ -27,7 +31,16 @@ describe('ContextMenuDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should trigger menu to open', () => {
+    const menuTriggerSpy = jest.spyOn(component.menuTrigger, 'open');
+    component.ngAfterContentChecked();
+    expect(menuTriggerSpy).toHaveBeenCalled();
+  });
+
+  it('should close menu on document click', () => {
+    const contexrSpy = jest.spyOn(contexrMock, 'close');
+    const mouseEvent = { clientX: 0, clientY: 0 } as MouseEvent;
+    component.closeOnClick(mouseEvent);
+    expect(contexrSpy).toHaveBeenCalled();
   });
 });
